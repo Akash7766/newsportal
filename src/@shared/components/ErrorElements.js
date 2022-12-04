ad58a4aa1188551c3e7c4b32cca7d4c53e61ebd0
Akash Rahman <mraakash01932@gmail.com>
2022-12-05 03:37:53 +0600

    404 implements

--- src/@shared/components/ErrorElements.js
+++ src/@shared/components/ErrorElements.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { FaAngleLeft } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const ErrorElements = () => {
   return (
     <div className="h-screen flex justify-center items-center">
-      Something went wrong
+       <div className="flex justify-center items-center">
+            <Link to="/">
+                <FaAngleLeft class="fa-solid fa-arrow-left text-3xl font-bold"/>
+            </Link>  
+            <p>Something went wrong</p>
+       </div>
     </div>
   );
 };
