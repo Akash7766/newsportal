c890619e34800de98b32128e00fc0fe1c2b44f6d
Akash Rahman <mraakash01932@gmail.com>
2022-12-05 07:22:24 +0600

    post details complete

--- src/@modules/categories/routes/PostDetailsRoute.js
+++ src/@modules/categories/routes/PostDetailsRoute.js
@@ -4,6 +4,7 @@ import { Link, useLocation, useParams } from 'react-router-dom';
 import MixCard from '../../../@shared/components/MixCard';
 import RactangleCard from '../../../@shared/components/RactangleCard';
 import SectionHeader from '../../../@shared/components/SectionHeader';
+import SquareCard from '../../../@shared/components/SquareCard';
 import PostDetails from '../components/PostDetails';
 
 const PostDetailsRoute = () => {
@@ -36,16 +37,36 @@ const PostDetailsRoute = () => {
                         </div>
                     </div>
                     <PostDetails/>
+                    <div className='mt-10'>
+                        <div>
+                            <h2 className="mb-4">আরও পড়ুন</h2>
+                            <div className='grid sm:grid-cols-2 gap-5'>
+                                <RactangleCard/>
+                                <RactangleCard/>
+                            </div>
+                        </div>
+                        
+                    </div>
                 </div>
                 <div className='md:w-96 custom-border'>
                     <div className='grid gap-5 my-5'>
-                        <img src='https://tpc.googlesyndication.com/simgad/6035381064038992771' alt=''/>
-                        <img src='https://tpc.googlesyndication.com/simgad/6035381064038992771' alt=''/>
+                        <img className='w-full' src='https://tpc.googlesyndication.com/simgad/6035381064038992771' alt=''/>
+                        <img className='w-full' src='https://tpc.googlesyndication.com/simgad/6035381064038992771' alt=''/>
                     </div>
                    <MixCard/>
                    <MixCard/>
                    <MixCard/>
                    <MixCard/>
+                </div>
+            </div>
+            
+            <div className='mt-10'>
+                <h2 className="mb-4">আরও পড়ুন</h2>
+                <div className='grid sm:grid-cols-2 md:grid-cols-4 gap-5'>
+                    <SquareCard/>
+                    <SquareCard/>
+                    <SquareCard/>
+                    <SquareCard/>
                 </div>
             </div>
         </div>
--- src/@shared/layout/Layout.js
+++ src/@shared/layout/Layout.js
@@ -7,7 +7,7 @@ const Layout = () => {
     const location = useLocation()
     const removeFooterPaths = location.pathname === "/user/login" || location.pathname === "/user/signup" || location.pathname === "/user/reset" 
     return (
-        <div className='container mx-auto'>
+        <div className='container mx-auto px-3 sm:px-0'>
             {
                 removeFooterPaths || <Navigation/>
             }
