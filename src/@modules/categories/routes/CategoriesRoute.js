18713016e5c42816fe63d9900ab9be95b55aaf3c
Akash Rahman <mraakash01932@gmail.com>
2022-12-05 17:25:42 +0600

    category route implements

--- src/@modules/categories/routes/CategoriesRoute.js
+++ src/@modules/categories/routes/CategoriesRoute.js
@@ -1,9 +1,58 @@
 import React from 'react';
+import MainCard from '../../../@shared/components/MainCard';
+import PortaitCard from '../../../@shared/components/PortaitCard';
+import RactangleCard from '../../../@shared/components/RactangleCard';
+import TextCard from '../../../@shared/components/TextCard';
 
 const CategoriesRoute = () => {
     return (
         <div>
-            This is categories
+            <div className='flex justify-center my-5'>
+                <img src='https://tpc.googlesyndication.com/simgad/3217729136485671980' alt='ad images'/>
+            </div>
+            {/* news and ad  */}
+            <div className='grid  lg:grid-cols-4 gap-5'>
+                <div className='lg:col-span-3'>
+                    {/* news  */}
+                    <div className='grid md:grid-cols-3 gap-5'>
+                        <div className='col-span-2'>
+                            <MainCard/>
+                        </div>
+                        <div className='col-span-1'>
+                            <PortaitCard/>
+                        </div>
+                    </div>
+                    <div className='grid sm:grid-cols-3 gap-5'>
+                        <PortaitCard/>
+                        <PortaitCard/>
+                        <PortaitCard/>
+                    </div>
+                </div>
+                <div className='col-span-1 custom-border'>
+                    <div className='mb-5'>
+                        <img className='w-full' src='https://tpc.googlesyndication.com/simgad/6035381064038992771' alt=''/>
+                    </div>
+                    <div className='grid grid-cols-2 lg:grid-cols-1 gap-5 custom-border'>
+                        <TextCard serial="1"/>
+                        <TextCard serial="2"/>
+                        <TextCard serial="3"/>
+                        <TextCard serial="4"/>
+                        <TextCard serial="5 "/>
+                    </div>
+                </div>
+            </div>
+            <div className='flex justify-center my-5'>
+                <img src='https://tpc.googlesyndication.com/simgad/17825197492991032054' alt=''/>
+            </div>
+            <div className='custom-border sm:w-[500px] mx-auto my-10'>
+                <RactangleCard/>
+                <RactangleCard/>
+                <RactangleCard/>
+                <RactangleCard/>
+                <div className='text-center'>
+                    <button className='btn btn-xs'>Load more ...</button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/@shared/components/TextCard.js
+++ src/@shared/components/TextCard.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const TextCard = ({link='/category/football/id'}) => {
+const TextCard = ({link='/category/football/id' , serial="0"}) => {
+    console.log(serial);
+    if (serial > 0 ){
+        return <div className='mb-3 pb-3 custom-card custom-border-bottom'>
+                    <Link to={link}>
+                        <div className='flex items-center'>
+                            <h1 className='text-4xl text-gray-500 font-semibold inline-block px-3'>{serial}</h1>
+                            <h2 className="text-md lg:text-xl transition-all">শোয়েব মালিকের সঙ্গে সম্পর্ক নিয়ে মুখ খুললেন সেই পাকিস্তানি অভিনেত্রী</h2>
+                        </div>
+                    </Link>
+                </div>
+    }
     return (
         <div className='mb-3 pb-3 custom-card custom-border-bottom'>
                 <Link to={link}>
-                    <h2 className="text-md lg:text-xl transition-all">শোয়েব মালিকের সঙ্গে সম্পর্ক নিয়ে মুখ খুললেন সেই পাকিস্তানি অভিনেত্রী</h2>
+                    <div>
+                        <h2 className="text-md lg:text-xl transition-all">শোয়েব মালিকের সঙ্গে সম্পর্ক নিয়ে মুখ খুললেন সেই পাকিস্তানি অভিনেত্রী</h2>
+                    </div>
                 </Link>
             </div>
     );
